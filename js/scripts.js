c49533a84ad0ea168249b9697929ecdecfda9036
Jonas Winter <jonas_winter@hotmail.de>
2021-01-15 18:04:26 +0100

    Increased time per team member

--- js/scripts.js
+++ js/scripts.js
@@ -66,7 +66,7 @@
     /* Card Slider - Swiper */
 	var cardSlider = new Swiper('.card-slider', {
 		autoplay: {
-            delay: 4000,
+            delay: 8000,
             disableOnInteraction: false
 		},
         loop: true,
@@ -88,50 +88,6 @@
             }
         }
     });
-
-
-    /* Image Slider - Swiper */
-    var imageSlider = new Swiper('.image-slider', {
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: false
-		},
-        loop: false,
-        navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-        spaceBetween: 30,
-        slidesPerView: 5,
-		breakpoints: {
-            // when window is <= 380px
-            380: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            },
-            // when window is <= 516px
-            516: {
-                slidesPerView: 2,
-                spaceBetween: 10
-            },
-            // when window is <= 768px
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            },
-            // when window is <= 992px
-            992: {
-                slidesPerView: 4,
-                spaceBetween: 30
-            },
-            // when window is <= 1200px
-            1200: {
-                slidesPerView: 5,
-                spaceBetween: 30
-            },
-        }
-    });
-
 
     /* Image Slider - Magnific Popup */
 	$('.popup-link').magnificPopup({
